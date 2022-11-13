6ceb5f50556d295d9af267c0d4dbebe73173810b
MihiroH <mihiro.hashimoto.1125@gmail.com>
2022-11-13 16:36:47 +0900

    Tutorial Golang

---
+++ util/random.go
@@ -0,0 +1,45 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt generages a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of length n
+func RandomString(n int) string {
+	b := make([]byte, n)
+	k := len(alphabet)
+
+	for i := range b {
+		b[i] = alphabet[rand.Intn(k)]
+	}
+	return string(b)
+}
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney generates a random amount of money
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+// RandomCurrency generates a random currency code
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
